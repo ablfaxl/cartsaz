3a48b5c51e468339be348f2e5766a739701251ac
ablfaxl <taghavi.a2023@gmail.com>
2024-04-16 00:33:04 +0330

    feat: update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import { Colors } from '@/app/_components/colors/colors'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Colors />
+      home page
     </main>
   )
 }
