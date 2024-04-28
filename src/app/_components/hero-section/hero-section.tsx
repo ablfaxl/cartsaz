2a7f2494ba3151f6cb2f8fe17f7477caebfefb96
ablfaxl <taghavi.a2023@gmail.com>
2024-04-28 23:20:42 +0330

    chore: add hero-section.tsx

---
+++ src/app/_components/hero-section/hero-section.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const HeroSection: React.FC = () => {
+  return (
+    <div className="hero">
+      <h1 className="hero-title">جلسه اول</h1>
+      <h1>Lorem ipsum dolor sit amet consectetur.</h1>
+    </div>
+  )
+}
