722e2ce01769d3176e066dd86c87ff5aa2b1fdd1
ablfaxl <taghavi.a2023@gmail.com>
2024-04-29 16:15:25 +0330

    feat: add document page

---
+++ src/app/(documents)/docs/page.tsx
@@ -0,0 +1,13 @@
+export default function DocumentsPage() {
+  return (
+    <div className="document">
+      <div className="w-full flex flex-col justify-start items-center ml-10">
+        <h1 className="document-heading">Next js Folder Structure</h1>
+        <article className="document-body">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique,
+          non?
+        </article>
+      </div>
+    </div>
+  )
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+export default function NotFoundPage() {
+  return (
+    <div
+      lang="en"
+      className="w-screen h-screen flex items-center justify-center"
+    >
+      <h1 className="uppercase font-semibold font-sans leading-7">
+        404 | not found page
+      </h1>
+    </div>
+  )
+}
